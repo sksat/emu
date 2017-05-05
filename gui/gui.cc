958da8d15e772cdb8890b04d5ac40b74897dac87
sksat <sksat.tec@gmail.com>
2017-05-05 17:25:26 +0900

    GLFW callback test and draw display

--- gui/gui.cc
+++ gui/gui.cc
@@ -12,28 +12,68 @@ Gui::Gui(){
 }
 
 Gui::~Gui(){
-	
+	th_flg = false;
+	gui_th->join();
+//	for(;!th_flg;);
+	cerr<<"neko!w"<<endl;
 }
 
 void Gui::Init(){
+	disp = nullptr;
+
 	scrnx = DEFAULT_SCRNX;
 	scrny = DEFAULT_SCRNY;
+
+	//threadの初期化も行うべき
 }
 
 int Gui::Start(){
+	th_flg = true;
 	gui_th = new thread(&Gui::gui_proc, this);
+}
+
+void errCallBack(int e, const char *d){
+	cerr<<"errcode="<<e<<" description:"<<d<<endl;
+}
+
+static int hoge = 0;
+
+void keyCallBack(GLFWwindow *win, int key, int scancode, int act, int mod){
+	cerr<<"key:"<<key<<endl;
+	glfwMakeContextCurrent(win);
+	hoge++;
+	int i = hoge%3;
+	if(i==0) glClearColor(1.0, 0.0, 0.0, 1.0);
+	if(i==1) glClearColor(0.0, 1.0, 0.0, 1.0);
+	if(i==2) glClearColor(0.0, 0.0, 1.0, 1.0);
 }
 
 void Gui::gui_proc(){
 	GLFWwindow *win;
 
+	glfwSetErrorCallback(errCallBack);
 	glfwInit();
 
 	win = glfwCreateWindow(scrnx, scrny, "window", NULL, NULL);
+	glfwSetKeyCallback(win, keyCallBack);
 
 	glfwMakeContextCurrent(win);
 
-	while(!glfwWindowShouldClose(win)){
+//	glRasterPos2i(-1,1);
+
+	int i= hoge % 3;
+	if(i == 0) glClearColor(1.0, 0.0, 0.0, 1.0);
+	if(i == 1) glClearColor(0.0, 1.0, 0.0, 1.0);
+	if(i == 2) glClearColor(0.0, 0.0, 1.0, 1.0);
+	hoge++;
+
+	while(!glfwWindowShouldClose(win) && th_flg){
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		if(disp != nullptr){
+			glDrawPixels(scrnx, scrny, GL_RGB, GL_UNSIGNED_BYTE, disp->GetImage());
+		}
+
 		glfwSwapBuffers(win);
 		glfwWaitEvents();
 	}
@@ -42,6 +82,9 @@ void Gui::gui_proc(){
 
 	glfwDestroyWindow(win);
 //	glfwTerminate();
+
+	th_flg = true;
+
 	return;
 }
 
--- gui/gui.h
+++ gui/gui.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <thread>
+#include "../device/display.h"
 
 #define DEFAULT_SCRNX 320
 #define DEFAULT_SCRNY 200
@@ -15,11 +16,15 @@ public:
 	void Init();
 	int Start();
 
+	void SetDisplay(Display *disp){ this->disp = disp; }
+
 	void gui_proc();
 private:
 	static std::vector<Gui*> instances;
 	std::thread *gui_th;
+	bool th_flg;
 	int scrnx, scrny;
+	Display *disp;
 };
 
 #endif
