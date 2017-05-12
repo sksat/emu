f383b8dc11a96475b1d8e3a1850abdf8326ec87a
sksat <sksat.tec@gmail.com>
2017-05-12 23:39:04 +0900

    support initialize multi-arch

--- _emulator.h
+++ _emulator.h
@@ -1,8 +1,13 @@
 #ifndef EMULATOR_H_
 #define EMULATOR_H_
 
-#include "emulator_base.h"
+#include "common.h"
 #include "arch/arch.h"
+#include "insn_base.h"
+#include "emulator_base.h"
+#include "memory.h"
+
+#define DEFAULT_MEMORY_SIZE	1 * MB
 
 #define CASE(a) case (static_cast<int>(a))
 
@@ -15,13 +20,27 @@ public:
 		return emu;
 	}
 
+//	EmulatorBase* operator->*(EmulatorBase* U::*) const noexcept ;
+
+	void Init(){
+		emu->Init();
+	}
+
 	void SetArch(int arch){
 		if(emu != nullptr) delete emu;
 		switch(arch){
 		CASE(ARCH::x86):
 			this->arch = ARCH::x86;
 			emu = new x86::Emulator();
+			insn = new x86::Instruction();
+			insn->Init();
 //			throw "x86";
+			break;
+		CASE(ARCH::osecpu):
+			this->arch = ARCH::osecpu;
+			emu = new osecpu::Emulator();
+			insn = new osecpu::Instruction();
+			insn->Init();
 			break;
 		default:
 			// unkown arch
@@ -29,8 +48,10 @@ public:
 		}
 		emu->Init();
 	}
+
 private:
 	ARCH arch;
+	InsnBase *insn;
 	EmulatorBase *emu;
 };
 
