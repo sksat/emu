7e5509b0396eaccb1d19148fc4bf1696ac670a5d
sksat <sksat.tec@gmail.com>
2017-07-29 17:28:07 +0900

    [UPDATE] test program

--- sample/startup.asm
+++ sample/startup.asm
@@ -2,6 +2,15 @@ BITS 32
 extern main
 global start
 ;	org 0x7c00
+
 start:
-	call main
+;	call main
+	mov dword [0x00], 10000000
+	call loop
 	jmp 0
+
+loop:
+	inc ecx
+	cmp ecx,[0x00]
+	jnz loop
+	ret
