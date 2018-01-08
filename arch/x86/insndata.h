726bfe2e62109f759b0ae20652b617d6140acc80
sksat <sksat.tec@gmail.com>
2018-01-08 19:46:13 +0900

    [FIX] parse ModR/M, [ADD] setter & getter for R/RM

--- arch/x86/insndata.h
+++ arch/x86/insndata.h
@@ -64,35 +64,24 @@ public:
 			if(RM == 0b110)
 				goto get_disp16;
 			else
-				goto not_impl;
+				break;
 		case 0b01:
 			goto get_disp8;
 		case 0b10:
 			goto get_disp16;
 		case 0b11:
-			goto not_impl;
+			break;
 get_disp8:
 	disp8 = static_cast<int8_t>(emu->GetCode8(0));
 	emu->EIP++;
-	DOUT("disp8");
+	DOUT("disp8: 0x"<<std::hex<<static_cast<uint32_t>(disp8)<<std::endl);
 	break;
 get_disp16:
 	disp16 = static_cast<int16_t>(emu->GetCode32(0));
 	emu->EIP+=2;
-	DOUT("disp16");
+	DOUT("disp16: 0x"<<std::hex<<disp16<<std::endl);
 	break;
-not_impl:
-			{
-				std::stringstream ss;
-				ss << "not implemented: Mod=0x" << std::hex
-					<< static_cast<uint32_t>(MOD)
-					<< ", R/M="
-					<< static_cast<uint32_t>(RM)
-					<< std::endl;
-				throw ss.str();
-			}
 		}
-		DOUT(": 0x"<<std::hex<<disp16<<std::endl);
 	}
 
 	inline void ParseModRM32(){
@@ -120,7 +109,7 @@ not_impl:
 		case 0b10:
 			goto get_disp32;
 		case 0b11:
-			goto not_impl;
+			break;
 get_disp8:
 	disp8 = static_cast<int8_t>(emu->GetCode8(0));
 	emu->EIP++;
@@ -131,41 +120,40 @@ get_disp32:
 	emu->EIP+=4;
 	DOUT("disp32");
 	break;
-not_impl:
-			{
-				std::stringstream ss;
-				ss << "not implemented: Mod=0x" << std::hex
-					<< static_cast<uint32_t>(MOD)
-					<< ", R/M="
-					<< static_cast<uint32_t>(RM)
-					<< std::endl;
-				throw ss.str();
-			}
 		}
 		DOUT(": 0x"<<std::hex<<disp32<<std::endl);
 	}
 
 	inline uint32_t CalcMemAddr(){
-		switch(modrm.mod){
+		if(emu->IsMode16())
+			return CalcMemAddr16();
+		else
+			return CalcMemAddr32();
+	}
+	inline uint32_t CalcMemAddr16(){
+		throw "not implemented: CalcMemAddr16";
+	}
+	inline uint32_t CalcMemAddr32(){
+		switch(MOD){
 			case 0:
-				switch(modrm.rm){
+				switch(RM){
 					case 4:
 						throw "not implemented ModRM mod=0, rm=4";
 					case 5:
 						return disp32;
 					default:
-						return emu->reg[modrm.rm];
+						return emu->reg[RM];
 				}
 				break;
 			case 1:
-				if(modrm.rm == 4){
+				if(RM == 4){
 					throw "not implemented ModRM mod=1, rm=4";
 				}else{
-					return emu->reg[modrm.rm] + disp8;
+					return emu->reg[RM] + disp8;
 				}
 				break;
 			case 2:
-				if(modrm.rm == 4){
+				if(RM == 4){
 					throw "not implemented ModRM mod=2, rm=4";
 				}else{
 					return emu->reg[modrm.rm] + disp32;
@@ -179,21 +167,88 @@ not_impl:
 				break;
 		}
 	}
+
+	// get register
+	inline uint8_t GetR8(){
+		return emu->reg[modrm.reg];
+	}
+	inline uint16_t GetR16(){
+		return emu->reg[modrm.reg];
+	}
+	inline uint32_t GetR32(){
+		return emu->reg[modrm.reg];
+	}
+
+	// set register
+	inline void SetR8(uint8_t val){
+		emu->reg[modrm.reg] = val;
+	}
+	inline void SetR16(uint16_t val){
+		emu->reg[modrm.reg] = val;
+	}
+	inline void SetR32(uint32_t val){
+		emu->reg[modrm.reg] = val;
+	}
+
+	// get rm
+	inline uint8_t GetRM8(){
+		if(MOD == 3)
+			return emu->reg[RM];
+		auto addr = CalcMemAddr();
+		DOUT("GetRM8: addr=0x"<<std::hex<<addr<<std::endl);
+		return emu->memory->GetData8(addr);
+	}
+	inline uint16_t GetRM16(){
+		if(MOD == 3)
+			return emu->reg[RM];
+		auto addr = CalcMemAddr();
+		DOUT("GetRM16: addr=0x"<<std::hex<<addr<<std::endl);
+//		return emu->memory->GetData16(addr);
+		throw "not implemented: memory::GetData16";
+	}
 	inline uint32_t GetRM32(){
-		if(modrm.mod == 3)
+		if(MOD == 3)
 			return emu->reg[modrm.rm];
-		DOUT("GetRM32: mem addr ="<<std::hex<<CalcMemAddr()<<std::endl);
-		return emu->memory->GetData32(CalcMemAddr());
+		auto addr = CalcMemAddr();
+		DOUT("GetRM32: addr=0x"<<std::hex<<addr<<std::endl);
+		return emu->memory->GetData32(addr);
+	}
+
+	// set rm
+	inline void SetRM8(uint8_t val){
+		if(MOD == 3){
+			emu->reg[RM] = val;
+			DOUT("SetRM8: "<<emu->reg[RM].GetName()
+					<<" = 0x"<<std::hex<<static_cast<uint32_t>(val)<<std::endl);
+			return;
+		}
+		auto addr = CalcMemAddr();
+		emu->memory->SetData8(addr, val);
+		DOUT("SetRM8: [0x"<<std::hex<<addr
+				<<"] = 0x"<<static_cast<uint32_t>(val)<<std::endl);
+	}
+	inline void SetRM16(uint16_t val){
+		if(MOD == 3){
+			emu->reg[RM] = val;
+			DOUT("SetRM16: "<<emu->reg[RM].GetName()<<" = 0x"<<std::hex<<val<<std::endl);
+			return;
+		}
+		auto addr = CalcMemAddr();
+//		emu->memory->SetData16(addr, val);
+		DOUT("SetRM16: [0x"<<std::hex<<addr
+				<<"] = 0x"<<val<<std::endl);
+		throw "not implemented: memory::SetData16";
 	}
 	inline void SetRM32(uint32_t val){
 		if(modrm.mod == 3){
-			emu->reg[modrm.rm] = val;
-			DOUT("SetRM32: reg="<<emu->reg[modrm.rm].GetName()<<" val="<<std::hex<<val<<std::endl);
-		}else{
-			uint32_t addr = CalcMemAddr();
-			emu->memory->SetData32(addr, val);
-			DOUT("SetRM32: addr="<<std::hex<<addr<<" val="<<val<<std::endl);
+			emu->reg[RM] = val;
+			DOUT("SetRM32: reg="<<emu->reg[RM].GetName()<<" val="<<std::hex<<val<<std::endl);
+			return;
 		}
+		uint32_t addr = CalcMemAddr();
+		emu->memory->SetData32(addr, val);
+		DOUT("SetRM32: [0x"<<std::hex<<addr
+				<<"] = 0x"<<val<<std::endl);
 	}
 
 };
