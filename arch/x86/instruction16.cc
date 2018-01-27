d33186d3de2f3215b92c15ac9036e8062dfe47a1
sksat <sksat.tec@gmail.com>
2018-01-27 16:08:29 +0900

    [ADD] impl add_rm16_imm8(code_83)

--- arch/x86/instruction16.cc
+++ arch/x86/instruction16.cc
@@ -6,7 +6,8 @@ namespace x86 {
 
 void Instruction16::Init(){
 	Instruction::Init();
-	
+
+	SETINSN(0x83, code_83, Flag::ModRM | Flag::Imm8);
 	for(auto i=0;i<8;i++)
 		SETINSN(0xb8+i, mov_r16_imm16, Flag::Imm16);
 }
--- arch/x86/instruction16.h
+++ arch/x86/instruction16.h
@@ -10,6 +10,26 @@ public:
 	Instruction16(x86::Emulator *e) : x86::Instruction(e) {}
 	void Init();
 
+	void add_rm16_imm8(){
+		DOUT("\n\t"<<__func__<<" ");
+		uint16_t rm16 = idata->GetRM16();
+		uint16_t result = rm16 + idata->imm8;
+		idata->SetRM16(result);
+		EFLAGS.UpdateAdd(rm16, idata->imm8, result);
+	}
+	void code_83(){
+		DOUT(" REG="<<static_cast<uint32_t>(idata->modrm.reg)<<" ");
+		switch(idata->modrm.reg){
+		case 0:	add_rm16_imm8();	break;
+		default:
+			{
+				std::stringstream ss;
+				ss << "not implemented: 0x83 /"<<static_cast<uint32_t>(idata->modrm.reg);
+				throw ss.str();
+			}
+		}
+	}
+
 	void mov_r16_imm16(){
 		uint8_t num = idata->opcode & ((1<<3)-1);
 		auto& reg = emu->reg[num];
