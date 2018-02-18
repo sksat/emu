c5068bdbe9d95cf831be7f3062cc0d94c33c360e
sksat <sksat.tec@gmail.com>
2018-02-18 22:11:41 +0900

    [ADD] Device::Keyboard::out8(temporary)

--- device/keyboard.h
+++ device/keyboard.h
@@ -13,6 +13,10 @@ public:
 		DOUT(std::endl<<"Keyboard::in8 port="<<std::hex<<port<<" is implemented temporary"<<std::endl);
 		return 0x00;
 	}
+
+	void out8(const uint16_t &port, const uint8_t &val){
+		DOUT(std::endl<<"Keyboard::out8 port="<<std::hex<<port<<", val="<<static_cast<uint32_t>(val)<<" is implemented temporary"<<std::endl);
+	}
 };
 
 }
