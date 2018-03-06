1d024b9411148fad48ee9dbb208547046d6af98d
sksat <sksat.tec@gmail.com>
2018-03-07 00:30:15 +0900

    [FIX] 0x81:sub_rm32_imm32 -> 0x81:code_81(/5 sub_rm32_imm32)

--- arch/x86/instruction32.h
+++ arch/x86/instruction32.h
@@ -51,13 +51,22 @@ private:
 			EFLAGS.CF = EFLAGS.OF = 0;
 	}
 
-	void sub_rm32_imm32(){
-		uint32_t rm32 = idata->GetRM32();
-		int32_t set = static_cast<int32_t>(rm32) - idata->imm32;
-		DOUT(std::endl<<"neko: RM32=0x"<<std::hex<<static_cast<int32_t>(rm32)<<", imm32=0x"<<idata->imm32<<", set=0x"<<set);
-		idata->SetRM32(set);
-		EFLAGS.UpdateSub(rm32, idata->imm32, set);
+	void code_81(){
+		switch(idata->modrm.reg){
+		case 5: sub_rm32_imm32(); break;
+		default:
+			std::stringstream ss;
+			ss << "not implemented: 0x83 /" << static_cast<uint32_t>(idata->modrm.reg);
+			throw ss.str();
+		}
 	}
+		void sub_rm32_imm32(){
+			uint32_t rm32 = idata->GetRM32();
+			int32_t set = static_cast<int32_t>(rm32) - idata->imm32;
+			DOUT(std::endl<<"neko: RM32=0x"<<std::hex<<static_cast<int32_t>(rm32)<<", imm32=0x"<<idata->imm32<<", set=0x"<<set);
+			idata->SetRM32(set);
+			EFLAGS.UpdateSub(rm32, idata->imm32, set);
+		}
 
 	void code_83(){
 		switch(idata->modrm.reg){
