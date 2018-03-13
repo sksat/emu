f71b46ee280e2907a688282ebe62ced055c3e670
sksat <sksat.tec@gmail.com>
2018-03-13 14:32:57 +0900

    [ADD] EFLAGS::UpdateSal, [UPDATE] EFLAGS::UpdateShr

--- arch/x86/register.h
+++ arch/x86/register.h
@@ -169,6 +169,8 @@ public:
 
 	template<typename T>
 	inline void UpdateShr(T v1, uint8_t v2, uint64_t result){
+		if(v2 == 0) return;
+
 		auto size = sizeof(T)*8;
 		bool signr = (result >> (size-1)) & 1;
 
@@ -179,6 +181,22 @@ public:
 		// OFは1bitシフトのときのみ影響を受ける
 		if(v2 == 1)
 			SetOverflow((v1>>(size-1)) & 1);
+	}
+
+	template<typename T>
+	inline void UpdateSal(T dest, uint8_t count, uint64_t result){
+		if(count == 0) return;
+
+		auto size = sizeof(T)*8;
+		bool signr = (result >> (size-1)) & 1;
+
+		SetCarry((result>>size) & 1);
+		SetZero(result == 0);
+		SetSign(signr);
+
+		// OFは1bitシフトのときのみ影響を受ける
+		if(count == 1)
+			SetOverflow(CF != ((result>>(size-1)) & 1));
 	}
 
 	inline void UpdateXor(){
